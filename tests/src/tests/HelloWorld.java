c0f6801e2c82963f9f733ec4df5fdb95caa4e236
Kai Zhang <kyle1001001@gmail.com>
2021-04-20 20:49:35 -0700

    init3

--- tests/src/tests/HelloWorld.java
+++ tests/src/tests/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello world ... ");
+		System.out.print("Hello world ... 3 ");
 
 	}
 
